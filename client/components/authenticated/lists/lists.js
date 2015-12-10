de047b932c6fc6e1d3c9d6e4df0a2ef6b4e91b52
Devan Beitel <devan.beitel@gmail.com>
2015-12-10 01:56:39 -0600

    Added tabs for list types on My Lists.

--- client/components/authenticated/lists/lists.js
+++ client/components/authenticated/lists/lists.js
@@ -9,5 +9,35 @@ Template.lists.helpers({
     if (lists) {
       return lists;
     }
+  },
+  publicLists: function(){
+    var user  = Meteor.userId(),
+        lists = Lists.find({$and: [{"owner": user}, {"type": "public"}]});
+
+    if (lists) {
+      return lists;
+    }
+  },
+  privateLists: function(){
+    var user  = Meteor.userId(),
+        lists = Lists.find({$and: [{"owner": user}, {"type": "private"}]});
+
+    if (lists) {
+      return lists;
+    }
+  },
+  forTradeLists: function(){
+    var user  = Meteor.userId(),
+        lists = Lists.find({$and: [{"owner": user}, {"forTrade": true}]});
+
+    if (lists) {
+      return lists;
+    }
+  },
+  capitalize: function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+  forTrade: function(list) {
+    return list.forTrade === true ? "For Trade" : "Not For Trade"
   }
 });
