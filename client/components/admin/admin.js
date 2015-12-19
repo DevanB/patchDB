27cc8149aa5f1cf5cd7ea298a23b85d64bbfb83e
Devan Beitel <devan.beitel@gmail.com>
2015-12-19 09:21:04 -0600

    Added admin area route, template, triggers.

---
+++ client/components/admin/admin.js
@@ -0,0 +1,19 @@
+Template.admin.onCreated(() => {
+
+});
+
+Template.admin.onRendered(() => {
+
+});
+
+Template.admin.onDestroyed(() => {
+
+});
+
+Template.admin.helpers({
+
+});
+
+Template.admin.events({
+
+});
